9a01c15a01a9cb64a7eda22cb0f654ee0a3c3c07
titobahe <titobahe@gmail.com>
2025-06-12 00:37:35 +0300

    msgIcon

--- styleMensages.js
+++ styleMensages.js
@@ -10,5 +10,20 @@ style.innerHTML = `
     border-color: transparent;
     }
 
+    .messages-single.\--own-message .message-bubble {
+    background-color: #53d4df4f !important;
+    color: #060606;
+    }
+
+    .messages-single.\--own-message .message-bubble:before {
+    border-color: transparent;
+    }
+
+    .icon .icon-sms2{
+    background-image: url('https://titobahe.github.io/msgIcon.png') !important;
+    background-size: contain;
+    background-repeat: no-repeat;
+    }
+
 `;
 document.head.appendChild(style);
