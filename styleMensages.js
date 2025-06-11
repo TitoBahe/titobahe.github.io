bc441abb74d7cc549af3709d87cd0b336d6c0fb0
titobahe <titobahe@gmail.com>
2025-06-12 01:06:38 +0300

    2

--- styleMensages.js
+++ styleMensages.js
@@ -25,6 +25,7 @@ style.innerHTML = `
     background-repeat: no-repeat;
   }
     
+  
   .type-icon.icon.icon-sms2.absolute-left {
     background-image: url('https://titobahe.github.io/msgIcon.png') !important;
     background-size: contain;
